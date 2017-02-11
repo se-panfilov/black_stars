f73338a65f3ca706caa89ad6b38c106cefede0b5
Sergey Panfilov <se-panfilov@ya.ru>
2017-02-12 04:06:58 +0800

    initial commit

---
+++ android/src/com/blackwings/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.blackwings.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.blackwings.game.BlackStarsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BlackStarsGame(), config);
+	}
+}
