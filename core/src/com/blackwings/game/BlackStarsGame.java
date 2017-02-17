ed35509bc730b05cba5b27cbf17ac6aa975ef69e
Sergey Panfilov <se-panfilov@ya.ru>
2017-02-18 00:31:07 +0800

    added demo iteraction with libgdx

--- core/src/com/blackwings/game/BlackStarsGame.java
+++ core/src/com/blackwings/game/BlackStarsGame.java
@@ -3,8 +3,14 @@ package com.blackwings.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.blackwings.space.objects.PositionData;
 import com.blackwings.space.objects.ships.Cruiser;
 import com.blackwings.space.objects.ships.Ship;
@@ -13,13 +19,29 @@ import com.blackwings.space.objects.subsystems.engines.ImpulseEngine;
 import com.blackwings.space.objects.subsystems.weapons.RailGun;
 
 public class BlackStarsGame extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+    private SpriteBatch batch;
+    private Texture cruiserImg;
+    private OrthographicCamera camera;
+    private OrthographicCamera uiCamera;
+    private Array<Ship> shipsList;
+    private Animation<TextureRegion> cruiserAnimation;
+    private float cruiserStateTime = 0;
+    private Vector2 cruiserPosition = new Vector2();
+    private Vector2 cruiserVelocity = new Vector2();
+    private Texture background;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        uiCamera = new OrthographicCamera();
+        uiCamera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        uiCamera.update();
+
+        background = new Texture("background.png");
 
         //Ship creation
         String name = "Nameless one";
@@ -32,23 +54,126 @@ public class BlackStarsGame extends ApplicationAdapter {
         subSystemsList.add(railGun);
         subSystemsList.add(impulseEngine);
 
-        Ship cruiser = new Cruiser(name, positionData, subSystemsList);
+        Cruiser cruiser = new Cruiser(name, positionData, subSystemsList);
+        shipsList.add(cruiser);
         //TODO (S.Panfilov) curWorkPoint
         //End Ship creation
+
+        cruiserImg = new Texture("cruiser.png");
+//        cruiserImg.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        cruiserAnimation = new Animation<TextureRegion>(0.05f, new TextureRegion(cruiserImg));
+        cruiserAnimation.setPlayMode(Animation.PlayMode.LOOP);
+
+        resetWorld();
+    }
+
+    private void resetWorld() {
+//        score = 0;
+//        groundOffsetX = 0;
+        cruiserPosition.set(PLANE_START_X, PLANE_START_Y);
+        cruiserVelocity.set(0, 0);
+//        gravity.set(0, GRAVITY);
+        shipsList.clear();
+        camera.position.x = 400;
+//
+//        rocks.clear();
+//        for(int i = 0; i < 5; i++) {
+//            boolean isDown = MathUtils.randomBoolean();
+//            rocks.add(new Rock(700 + i * 200, isDown?480-rock.getRegionHeight(): 0, isDown? rockDown: rock));
+//        }
+    }
+
+    private void updateWorld() {
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        cruiserStateTime += deltaTime;
+
+        if (Gdx.input.justTouched()) {
+//            if(gameState == GameState.Start) {
+//                gameState = GameState.Running;
+//            }
+//            if(gameState == GameState.Running) {
+            cruiserVelocity.set(PLANE_VELOCITY_X, PLANE_JUMP_IMPULSE);
+//            }
+//            if(gameState == GameState.GameOver) {
+//                gameState = GameState.Start;
+//                resetWorld();
+//            }
+        }
+
+        if (gameState != GameState.Start) cruiserVelocity.add(gravity);
+
+        cruiserPosition.mulAdd(cruiserVelocity, deltaTime);
+
+        camera.position.x = cruiserPosition.x + 350;
+        if (camera.position.x - groundOffsetX > ground.getRegionWidth() + 400) {
+            groundOffsetX += ground.getRegionWidth();
+        }
+
+        rect1.set(cruiserPosition.x + 20, cruiserPosition.y, plane.getKeyFrames()[0].getRegionWidth() - 20, plane.getKeyFrames()[0].getRegionHeight());
+        for (Rock r : rocks) {
+            if (camera.position.x - r.position.x > 400 + r.image.getRegionWidth()) {
+                boolean isDown = MathUtils.randomBoolean();
+                r.position.x += 5 * 200;
+                r.position.y = isDown ? 480 - rock.getRegionHeight() : 0;
+                r.image = isDown ? rockDown : rock;
+                r.counted = false;
+            }
+            rect2.set(r.position.x + (r.image.getRegionWidth() - 30) / 2 + 20, r.position.y, 20, r.image.getRegionHeight() - 10);
+            if (rect1.overlaps(rect2)) {
+                if (gameState != GameState.GameOver) explode.play();
+                gameState = GameState.GameOver;
+                cruiserVelocity.x = 0;
+            }
+            if (r.position.x < cruiserPosition.x && !r.counted) {
+                score++;
+                r.counted = true;
+            }
+        }
+
+        if (cruiserPosition.y < ground.getRegionHeight() - 20 ||
+                cruiserPosition.y + plane.getKeyFrames()[0].getRegionHeight() > 480 - ground.getRegionHeight() + 20) {
+            if (gameState != GameState.GameOver) explode.play();
+            gameState = GameState.GameOver;
+            cruiserVelocity.x = 0;
+        }
+    }
+
+    private void drawWorld() {
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(background, camera.position.x - background.getWidth() / 2, 0);
+
+//        for(Ship ship: shipsList) {
+//            batch.draw(ship.image, ship.position.x, ship.position.y);
+//        }
+
+        batch.draw(cruiserAnimation.getKeyFrame(cruiserStateTime), cruiserPosition.x, cruiserPosition.y);
+        batch.end();
+
+        batch.setProjectionMatrix(uiCamera.combined);
+        batch.begin();
+//        if (gameState == GameState.Start) {
+//            batch.draw(ready, Gdx.graphics.getWidth() / 2 - ready.getRegionWidth() / 2, Gdx.graphics.getHeight() / 2 - ready.getRegionHeight() / 2);
+//        }
+        batch.end();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+//        batch.begin();
+//        batch.draw(cruiserImg, 0, 0);
+//        batch.end();
+
+        updateWorld();
+        drawWorld();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        cruiserImg.dispose();
     }
 }
