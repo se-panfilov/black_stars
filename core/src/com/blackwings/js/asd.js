7db4181e4f01ea516ce68af58dd1c46fe76f476c
Sergei <se-panfilov@ya.ru>
2017-06-05 00:54:53 +0800

    experiments

--- core/src/com/blackwings/js/asd.js
+++ core/src/com/blackwings/js/asd.js
@@ -1,6 +1,9 @@
+var dsa = require('/Users/sparex2/work/black_stars/core/src/com/blackwings/js/dsa.js');
+
 var asd = {
     doSomething: function  () {
         print('111111111');
+        dsa.doOther();
     }
 }
 
